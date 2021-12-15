27df2f0a9b7e9c510fc77b7cfa61ae491d2bd6aa
imstar15 <imstar15@qq.com>
2021-12-15 20:02:33 +0800

    Test contribute

--- src/App.js
+++ src/App.js
@@ -33,6 +33,27 @@ function App() {
 		console.log("txHex: ", signedExtrinsic.toHex());
 		console.log("txHash: ", signedExtrinsic.hash.toString());
   }
+  
+  const testContribute = async () => {
+    await web3Enable('oak-parse');
+
+    const account = '5GcD1vPdWzBd3VPTPgVFWL9K7b27A2tPYcVTJoGwKcLjdG5w';
+		const fundAmount = 100;
+
+		const injector = await web3FromAddress(account);
+		const polkadotApi = polkadotApiHelper.getApi();
+		// const extrinsic = api.tx.quadraticFunding.contribute(0, fundAmount * 10 ** 10);
+		const extrinsic = polkadotApi.tx.quadraticFunding.fund(fundAmount * 10 ** 10);
+
+		const signedExtrinsic = await extrinsic.signAsync(account, { signer: injector.signer });
+    console.log("signedExtrinsic: ", signedExtrinsic);
+    console.log("signer: ", signedExtrinsic.signer.toString());
+		console.log("txHex: ", signedExtrinsic.toHex());
+		console.log("txHash: ", signedExtrinsic.hash.toString());
+
+    const result = await Parse.Cloud.run("contribute", { "extrinsicHex": signedExtrinsic.toHex(), "chain": "Kusama" });
+    console.log('result: ', result);
+  }
 
   return (
     <div className="App">
@@ -51,6 +72,7 @@ function App() {
         </a>
         <Button className="App-test-button" type="primary" onClick={testParse}>Test parse</Button>
         <Button className="App-test-button" type="primary" onClick={testPolkadot}>Test Polkadot</Button>
+        <Button className="App-test-button" type="primary" onClick={testContribute}>Test Contribute</Button>
       </header>
     </div>
   );
