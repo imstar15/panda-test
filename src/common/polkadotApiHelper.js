4852f82364c4df3ec5d3b9c90a2e357cdcd235a7
imstar15 <imstar15@qq.com>
2021-12-16 12:02:36 +0800

    Test crowdloan

---
+++ src/common/polkadotApiHelper.js
@@ -0,0 +1,28 @@
+import config from "../config";
+import { WsProvider, ApiPromise } from '@polkadot/api';
+
+class PolkadotApiHelper {
+	constructor() {
+		this.init();
+		this.kusamaApi = null;
+	}
+
+	init = async () => {
+		const { endpoint, types } = config.polkadot.oak;
+		const wsProvider = new WsProvider(endpoint);
+		this.api = await ApiPromise.create({ provider: wsProvider, types });
+	}
+
+	getApi = () => this.api;
+
+	getKusamaApi = async () => {
+		if (!this.kusamaApi) {
+			const { endpoint } = config.polkadot.kusama;
+			const wsProvider = new WsProvider(endpoint);
+			this.kusamaApi = await ApiPromise.create({ provider: wsProvider });
+		}
+		return this.kusamaApi;
+	}
+}
+
+export default new PolkadotApiHelper();
